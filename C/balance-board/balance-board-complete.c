37e40d0b3b13acb6e13eb0e97fc0b6985aa7e606
Lucas Warwick <lucaswarwick02@gmail.com>
2022-05-12 20:41:52 +0100

    Add complete and incomplete solutions

--- C/balance-board/balance-board-complete.c
+++ C/balance-board/balance-board-complete.c
@@ -27,12 +27,24 @@ int main() {
     stdio_init_all();
     setvbuf(stdout, NULL, _IONBF, 0);
     sleep_ms(1000);
+    ST7735_Init();
+    ST7735_FillScreen(ST7735_WHITE);
 
-    // TODO: Setup the ST7735 on GPIO 4 and 5, using I2C
+    i2c_init(i2c0, 400 * 1000);
+    gpio_set_function(4, GPIO_FUNC_I2C);
+    gpio_set_function(5, GPIO_FUNC_I2C);
+
+    gpio_pull_up(4);
+    gpio_pull_up(5);
 
     sleep_ms(1000);
 
-    // TODO: Setup the IMU
+    IMU_EN_SENSOR_TYPE enMotionSensorType;
+    imuInit(&enMotionSensorType);
+    if (IMU_EN_SENSOR_TYPE_ICM20948 != enMotionSensorType) {
+        printf("Failed to initialise IMU...");
+    }
+    printf("IMU initialised!");
 
     float x;
     float y;
@@ -41,8 +53,9 @@ int main() {
     uint16_t colour = ST7735_GREEN;
 
     while (true) {
-        // TODO: Get, and assign, the values from the accelation of the IMU
-        // Hint: When assigning to x, y and z, use the absolute value!
+        icm20948AccelRead(&x, &y, &z);
+        x = getAbs(x);
+        y = getAbs(y);
 
         max = getMax(x, y);
         if (max < 0.33f) {
